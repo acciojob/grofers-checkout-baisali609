55d504532809ba432cacd5fd91d7ed03fd369950
baisali609 <baisali609@gmail.com>
2025-02-14 12:14:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,23 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	const prices = document.querySelectorAll(".price");
+    let total = 0;
+    
+    prices.forEach(price => {
+    total += parseFloat(price.textContent);
+    });
+
+	const table = document.querySelector("table");
+    const totalRow = document.createElement("tr");
+    const totalCell = document.createElement("td");
+    totalCell.textContent = `Total Price:  ${total}`;
+    totalRow.appendChild(totalCell);
+    
+    // Append total row to table
+    table.appendChild(totalRow);
 };
+
 
 getSumBtn.addEventListener("click", getSum);
 
